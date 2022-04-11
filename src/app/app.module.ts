2f6df31bb0c95715adae92c12b91a17a9da0999d
DanihStephane <92985744+DanihStephane@users.noreply.github.com>
2022-04-11 23:04:21 +0300

    simple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProduitsComponent } from "./produits/Produits.component";
+import { ProduitsComponent } from "./produits/produits.component";
 import { AddProduitComponent } from './add-produit/add-produit.component';
 import { FormsModule } from '@angular/forms';
 import { UpdateProduitComponent } from './update-produit/update-produit.component';
